b194b1e4c2ca4004fce5e7eca44b0d2b7ae37aee
twcoin <80656085+twcoin@users.noreply.github.com>
2024-06-15 16:49:28 +0800

    Create down.sh

---
+++ sh/down.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~ && touch acme.sh && chmod +x acme.sh
+ln -sf ~/acme.sh /usr/local/bin/zs
+githubusercontent_URL="https://raw.githubusercontent.com/twcoin/linux/main/sh/acme.sh"
+curl -L -O $githubusercontent_URL && chmod +x acme.sh
