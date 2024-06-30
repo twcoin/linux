89e4bd22182d7eb345ae4e6c6d66b503a1463b38
twcoin <80656085+twcoin@users.noreply.github.com>
2024-06-30 08:49:20 +0800

    Update down.sh

--- sh/down.sh
+++ sh/down.sh
@@ -3,4 +3,5 @@
 cd ~ && touch acme.sh && chmod +x acme.sh
 ln -sf ~/acme.sh /usr/local/bin/zs
 githubusercontent_URL="https://raw.githubusercontent.com/twcoin/linux/main/sh/acme.sh"
+giteebusercontent_URL="https://gitee.com/foxfix/linux/raw/master/sh/acme.sh"
 curl -L -O $githubusercontent_URL && chmod +x acme.sh
