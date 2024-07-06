26d7d274325ac3ef1df3a6be82b07d389fad5f5c
twcoin <80656085+twcoin@users.noreply.github.com>
2024-07-06 23:23:49 +0800

    Update down.sh

--- sh/down.sh
+++ sh/down.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #apt update && apt install -y wget curl htop file socat
-#proxy#proxy
-#export http_proxy="http://127.0.0.1:xx"
-#export https_proxy="http://127.0.0.1:xx"
+#proxy
+#export http_proxy="http://127.0.0.1:20172"
+#export https_proxy="http://127.0.0.1:20172"
 cd ~ && touch acme.sh && chmod +x acme.sh
 ln -sf ~/acme.sh /usr/local/bin/zs
 githubusercontent_URL="https://raw.githubusercontent.com/twcoin/linux/main/sh/acme.sh"
